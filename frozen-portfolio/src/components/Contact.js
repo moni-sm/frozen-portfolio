16891e177f0ce9219541d3183ebcfe5dda2f8749
moni-sm <152909890+moni-sm@users.noreply.github.com>
2025-10-04 07:20:43 +0530

    Update Contact.js

--- frozen-portfolio/src/components/Contact.js
+++ frozen-portfolio/src/components/Contact.js
@@ -21,7 +21,7 @@ const Contact = () => {
 
     // Send form data to the backend
     try {
-      const response = await fetch('http://localhost:5000/api/contact', {
+      const response = await fetch('https://meandmyself.onrender.com/api/contact', {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
